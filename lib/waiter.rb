ed910aa9f3f072c054c638c0c52e925e704d0334
Federico Buzeta Borquez <fbuzeta11@gmail.com>
2020-03-19 22:29:40 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -22,5 +22,13 @@ def new_meal(customer, total, tip=0)
       meal.waiter == self
     end
   end
+  
+  def best_tipper
+    best_tipped_meal = meals.max do |meal_a, meal_b|
+      meal_a.tip <=> meal_b.tip
+    end
+    best_tipped_meal.customer
+  end
+  
 
 end
